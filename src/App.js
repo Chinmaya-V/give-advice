e836854ed373b737700ffc9cd5ebb2814904d12f
Chinmaya-V <chinmayav04@gmail.com>
2023-12-03 00:23:16 +0530

    Added change background functionality

--- src/App.js
+++ src/App.js
@@ -3,29 +3,44 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-    state = { advice: "" };
+    state = { advice: "", loading: false, imageNo: 1 };
 
     componentDidMount() {
         this.fetchAdvice();
     }
 
     fetchAdvice = () => {
+        this.setState({ loading: true });
         axios
             .get("https://api.adviceslip.com/advice")
             .then((res) => {
                 const { advice } = res.data.slip;
-                this.setState({ advice });
+                this.setState({ advice, loading: false });
             })
-            .catch((err) => console.error(err));
+            .catch((err) => {
+                console.error(err);
+                alert(err.message);
+            });
     };
 
     render() {
+        const randomNumber = Math.floor(Math.random() * 20) + 1;
         return (
-            <div className="app">
+            <div
+                id="app"
+                className="app"
+                style={{
+                    backgroundImage: `url("${process.env.PUBLIC_URL}/images/backgroundImage${this.state.imageNo}.jpeg")`,
+                }}>
                 <div className="card">
                     <h1 className="heading">{this.state.advice}</h1>
-                    <button className="button" onClick={() => this.fetchAdvice()}>
-                        <span>I Need Advice</span>
+                    <button
+                        className="button"
+                        onClick={() => {
+                            this.fetchAdvice();
+                            this.setState({ imageNo: randomNumber });
+                        }}>
+                        <span>{this.state.loading ? "Loading..." : "I Need Advice"}</span>
                     </button>
                 </div>
             </div>
