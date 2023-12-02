626974a661644de12fbf7cdd81ae3cfa36a60e40
Chinmaya-V <chinmayav04@gmail.com>
2023-12-02 23:29:40 +0530

    Created Advice Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import axios from "axios";
 import React from "react";
+import "./App.css";
 
 class App extends React.Component {
+    state = { advice: "" };
+
+    componentDidMount() {
+        this.fetchAdvice();
+    }
+
+    fetchAdvice = () => {
+        axios
+            .get("https://api.adviceslip.com/advice")
+            .then((res) => {
+                const { advice } = res.data.slip;
+                this.setState({ advice });
+            })
+            .catch((err) => console.error(err));
+    };
+
     render() {
         return (
-            <div>
-                <h1>App</h1>
+            <div className="app">
+                <div className="card">
+                    <h1 className="heading">{this.state.advice}</h1>
+                    <button className="button" onClick={() => this.fetchAdvice()}>
+                        <span>I Need Advice</span>
+                    </button>
+                </div>
             </div>
         );
     }
